a13212acbef9897527bdcf701b52e0018bdbc7cc
Juan Roldan <juanroldan1989@gmail.com>
2016-05-22 12:57:05 +1200

    Seeds setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "Setting up sample orders..."
+
+15.times do |i|
+  Order.create({
+    phone: "(503)-444-1111",
+    section: ("250".."350").to_a.sample,
+    row:     ("A".."Z").to_a.sample,
+    seat:    ("A".."Z").to_a.sample,
+    status:  "pending"
+  })
+
+  print "."
+
+  sleep 5
+end
+
+4.times do |i|
+  Order.create({
+    phone: "(503)-444-1111",
+    section: ("250".."350").to_a.sample,
+    row:     ("A".."Z").to_a.sample,
+    seat:    ("A".."Z").to_a.sample,
+    status:  "done"
+  })
+
+  print "."
+
+  sleep 5
+end
+
+puts
+puts "Done !"
