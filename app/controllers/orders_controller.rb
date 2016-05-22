0b2851b91ece919e62fd89fe4e31439325223cee
Juan Roldan <juanroldan1989@gmail.com>
2016-05-22 15:35:52 +1200

    Counters updated when order is marked as done

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,11 +3,11 @@ class OrdersController < ApplicationController
   before_action :find_order
 
   helper_method :collection
+  helper_method :done
+  helper_method :pending
 
   def mark_as_done
     @order.done!
-
-    render nothing: :true, status: :ok
   end
 
   private
@@ -16,7 +16,15 @@ class OrdersController < ApplicationController
     @collection ||= Order.by_date.pending
   end
 
+  def done
+    Order.by_status("done").count
+  end
+
   def find_order
     @order = Order.find params[:id]
+  end
+
+  def pending
+    collection.count
   end
 end
