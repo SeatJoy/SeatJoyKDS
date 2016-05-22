433ae72074db635458d8d792097f0a535d964dab
Juan Roldan <juanroldan1989@gmail.com>
2016-05-22 12:57:24 +1200

    HomePage - Orders done/pending listing setup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,20 @@
 class HomeController < ApplicationController
 
+  before_action :set_counters
+
+  helper_method :orders
+
   def index
-    
+  end
+
+  private
+
+  def orders
+    @orders ||= Order.by_date.pending
+  end
+
+  def set_counters
+    @done    ||= Order.by_date.by_status("done").count
+    @pending ||= Order.by_date.pending.count
   end
 end
