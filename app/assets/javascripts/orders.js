086e8ad963426f61069332802f9eb66fdf980677
Juan Roldan <juanroldan1989@gmail.com>
2016-05-22 16:16:59 +1200

    Loader GIF setup when marking orders as Done

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -1,25 +1,3 @@
-$(document).ready(function(){
-  mark_as_done();
+$(document).ajaxStart(function(){
+  $("#loader").show();
 });
-
-function mark_as_done() {
-  $("[data-behavior~=mark-as-done]").on("click", function() {
-    var order_id = $(this).data("order-id")
-    var dom_id   = "#order_" + order_id;
-    // $("#loader").loader("show");
-
-    $.ajax({
-      type    : "PUT",
-      beforeSend: function(xhr){
-        xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))
-      },
-      url      : "/orders/" + order_id + "/mark_as_done",
-      data     : {},
-      success  : function(data){
-        $(dom_id).hide();
-        // $("#loader").loader("hide");
-      }
-    });
-
-  });
-}
