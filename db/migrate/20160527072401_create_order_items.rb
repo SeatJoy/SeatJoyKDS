77fa32eda667475a4919228ba289652322b76dc3
Juan Roldan <juanroldan1989@gmail.com>
2016-05-27 19:24:54 +1200

    OrderItem table added

---
+++ db/migrate/20160527072401_create_order_items.rb
@@ -0,0 +1,15 @@
+class CreateOrderItems < ActiveRecord::Migration
+  def change
+    create_table :order_items do |t|
+      t.integer :order_id
+      t.integer :qty
+      t.string  :title
+
+      t.timestamps null: false
+    end
+
+    add_index :order_items, :order_id
+    add_index :order_items, :qty
+    add_index :order_items, :title
+  end
+end
