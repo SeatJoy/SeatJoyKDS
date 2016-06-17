a5a9dd19bb25f1509a606fb9deeeb16bbddc7b55
Juan Roldan <juanroldan1989@gmail.com>
2016-06-17 19:36:22 +1200

    Orders list - listing by creation date in ascending way

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ActiveRecord::Base
 
-  scope :by_date,   ->          { order("created_at DESC") }
+  scope :by_date,   ->          { order("created_at ASC") }
   scope :by_status, -> (status) { where(status: status) }
   scope :pending,   ->          { where.not(status: "done") }
 
