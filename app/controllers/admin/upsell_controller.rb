6e53e6a4ba90c7b0d2aec0ec993d8f1581b0ea30
Juan Roldan <juanroldan1989@gmail.com>
2016-07-01 23:15:20 +1200

    Admin - Upsell - OrderItems checkboxes listing fetched from real OrderItems on Inventory section

--- app/controllers/admin/upsell_controller.rb
+++ app/controllers/admin/upsell_controller.rb
@@ -1,6 +1,12 @@
 class Admin::UpsellController < Admin::BaseController
 
+  helper_method :order_items
+
   private
+
+  def order_items
+    @order_items ||= InventoryOrderItem.all
+  end
 
   def set_nav_bar_klass
     @upsell_klass = "active"
