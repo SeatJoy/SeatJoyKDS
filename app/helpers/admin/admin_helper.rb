ca1e52aa51b994734241567b29b0ffb30a8bd6b5
Juan Roldan <juanroldan1989@gmail.com>
2016-07-01 22:33:16 +1200

    Admin Reports - CSV buttons removed - Time Range records setup properly

--- app/helpers/admin/admin_helper.rb
+++ app/helpers/admin/admin_helper.rb
@@ -4,4 +4,11 @@ module Admin::AdminHelper
     Time.now.strftime("%dth %B %Y")
   end
 
+  def fifteen_minutes_periods
+    @fifteen_minutes_periods ||=
+      (Time.parse("10:00 AM").to_i..Time.parse("09:00 PM").to_i)
+      .to_a.in_groups_of(15.minutes)
+      .collect(&:first)
+      .collect { |t| Time.at(t).strftime("%H:%M %p") }
+  end
 end
