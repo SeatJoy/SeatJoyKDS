f5d5ebb03e1fc52fe0a5e8226aeccf2e4b6dd1c1
Juan Roldan <juanroldan1989@gmail.com>
2016-07-09 23:10:48 +1200

    WickedPDF gem added - Initializer setup

---
+++ config/initializers/wicked_pdf.rb
@@ -0,0 +1,19 @@
+platform = RUBY_PLATFORM
+
+if Rails.env.production?
+  if platform.include?("darwin")      # OS X machine
+    binary_path = Rails.root.join("bin", "wkhtmltopdf-0.9.9-OS-X-i386").to_s
+  elsif platform.include?("64-linux") # 64-bit linux machine
+    binary_path = Rails.root.join("bin", "wkhtmltopdf-linux-amd64").to_s
+  end
+else
+  # binary_path = Rails.root.join("bin", "wkhtmltopdf-dev").to_s - when App is stored in linux partition. If it's stored on NTFS partition, this file can't be set as executable from a Linux environment.
+  binary_path = "/usr/local/bin/wkhtmltopdf-dev"
+end
+
+WickedPdf.config = {
+  :wkhtmltopdf => binary_path,
+  #:layout => "pdf.html",
+  # :exe_path => "/usr/bin/wkhtmltopdf" #path inside OS
+  :exe_path => binary_path #path inside Rails App
+}
