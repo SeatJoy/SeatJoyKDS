fb58cb606cff2bf7df0a99a9694dd17e54b176f3
Juan Roldan <juanroldan1989@gmail.com>
2016-07-12 21:58:41 +1200

    Admin - Reports - PDF JS files referenced properly

---
+++ app/assets/javascripts/pdf/application.js
@@ -0,0 +1,62 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+
+//= require admin/modernizr.custom
+//= require admin/bootstrap
+//= require admin/jquery.browser
+//= require admin/colors
+//= require admin/bootstrap-filestyle
+
+//= require admin/jquery.flot
+//= require admin/jquery.flot.categories
+//= require admin/jquery.flot.spline
+//= require admin/jquery.flot.tooltip
+//= require admin/jquery.flot.resize
+//= require admin/jquery.flot.pie
+//= require admin/jquery.flot.time
+//= require admin/jquery.flot.orderBars
+//= require admin/jquery-jvectormap-1.2.2.min
+//= require admin/jquery-jvectormap-us-mill-en
+//= require admin/jquery-jvectormap-world-mill-en
+//= require admin/jquery.easypiechart
+//= require admin/bootstrap-datepicker
+
+//= require admin/screenfull
+//= require admin/index
+//= require admin/d3
+//= require admin/rickshaw
+
+//= require admin/jquery.validate
+//= require admin/additional-methods
+
+//= require admin/nouislider
+
+//= require admin/summernote
+//= require admin/jquery.bootgrid
+//= require admin/jquery.bootgrid.fa
+//= require admin/masonry.pkgd
+
+//= require admin/loaders.css.js
+
+//= require admin/blueimp-helper
+//= require admin/blueimp-gallery
+//= require admin/blueimp-gallery-fullscreen
+//= require admin/blueimp-gallery-indicator
+//= require admin/blueimp-gallery-video
+//= require admin/blueimp-gallery-vimeo
+//= require admin/blueimp-gallery-youtube
+//= require admin/jquery.blueimp-gallery
+
+//= require admin/app
